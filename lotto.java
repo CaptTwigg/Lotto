ff6c5bddacd4ce0b99b3543d3d23fe7f3e7f6f0f
Mike Hemmje Jahn <bob@Mike-Hemmje-Jahn-Air.local>
2017-10-06 10:05:34 +0200

    Update get userNumber added

--- lotto.java
+++ lotto.java
@@ -3,6 +3,17 @@ import java.io.*;
 
 public class lotto {
 public static void main(String[] args) throws FileNotFoundException {
-	PrintStream getNumbers = new PrintStream(new File("numbers.dat"));
+	// PrintStream getNumbers = new PrintStream(new File("numbers.dat"));
+	userNumbers();
+}
+public static void userNumbers() throws FileNotFoundException {
+	Scanner getNumbers = new Scanner(new File("numbers.dat"));
+	int[] userArray = new int[6];
+	for (int i = 0; i < 6; i++) {
+		userArray[i] = getNumbers.nextInt();
+	}
+	for (int i : userArray) {
+		System.out.println(i);
+	}
 }
 }
