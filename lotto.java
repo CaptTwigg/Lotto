f47c0a2211a6abde804b303665e4c56d885f7176
CaptTwigg <mike@familien-jahn.dk>
2017-10-06 16:08:26 +0200

    Added comments for userNumbers and drawNumbers

--- lotto.java
+++ lotto.java
@@ -9,9 +9,9 @@ public static void main(String[] args) throws Exception {
 	// for (int i : userNumbers()) System.out.printf("%d ", i);
 }
 public static int[] userNumbers() throws Exception {
-	Scanner getNumbers = new Scanner(new File("numbers.dat"));
-	int[] userArray = new int[6];
-	for (int i = 0; i < 6; i++) userArray[i] = getNumbers.nextInt();
+	Scanner getNumbers = new Scanner(new File("numbers.dat"));     //Read file to get user numbers
+	int[] userArray = new int[6];                                  //Create array for lotto numbers with 6 indexes
+	for (int i = 0; i < 6; i++) userArray[i] = getNumbers.nextInt();        //Loop through array and add value from numbers.dat file
 	return userArray;
 }
 public static int compare() throws Exception {
@@ -27,17 +27,17 @@ public static int compare() throws Exception {
 public static void noUserDup(){
 }
 public static int[] drawNumbers() throws Exception {
-	Random rand = new Random();
-	int[] drawArray = new int [6];
+	Random rand = new Random();                             //random object declare
+	int[] drawArray = new int [6];          //Array for drawn numbers
 	for (int i = 0; i < 6; i++) {
-		drawArray[i] = rand.nextInt(20) + 1;
+		drawArray[i] = rand.nextInt(20) + 1;    //Add random value between 1-20 to array
 		for (int j = 0; j < i; j++) {
-			if (drawArray[i] == drawArray[j]) {
+			if (drawArray[i] == drawArray[j]) {     // check if numbers already exist -- yes jump 1 back in array and add new number
 				i--;
 			}
 		}
 	}
-	Arrays.sort(drawArray);
+	Arrays.sort(drawArray);         //Sort array for pleasure
 	return(drawArray);
 }
 }
