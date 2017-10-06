5a7a5f750595d0b46f607e88e681cca38f8b2ea2
Mike Hemmje Jahn <bob@Mike-Hemmje-Jahn-Air.local>
2017-10-06 09:55:21 +0200

    update

--- lotto.java
+++ lotto.java
@@ -1,5 +1,8 @@
+import java.util.*;
+import java.io.*;
+
 public class lotto {
-public static void main(String[] args) {
-	System.out.print("hej");
+public static void main(String[] args) throws FileNotFoundException {
+	PrintStream getNumbers = new PrintStream(new File("numbers.dat"));
 }
 }
