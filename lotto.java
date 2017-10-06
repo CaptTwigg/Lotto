c833532ee570b83176e6ffc6c649adc11e2d83a8
Mike Hemmje Jahn <bob@Mike-Hemmje-Jahn-Air.local>
2017-10-06 10:10:54 +0200

    Return userNumbers as array

--- lotto.java
+++ lotto.java
@@ -3,17 +3,15 @@ import java.io.*;
 
 public class lotto {
 public static void main(String[] args) throws FileNotFoundException {
-	// PrintStream getNumbers = new PrintStream(new File("numbers.dat"));
 	userNumbers();
-}
-public static void userNumbers() throws FileNotFoundException {
-	Scanner getNumbers = new Scanner(new File("numbers.dat"));
-	int[] userArray = new int[6];
-	for (int i = 0; i < 6; i++) {
-		userArray[i] = getNumbers.nextInt();
-	}
-	for (int i : userArray) {
+	for (int i : userNumbers()) {
 		System.out.println(i);
 	}
+}
+public static int[] userNumbers() throws FileNotFoundException {
+	Scanner getNumbers = new Scanner(new File("numbers.dat"));
+	int[] userArray = new int[6];
+	for (int i = 0; i < 6; i++) userArray[i] = getNumbers.nextInt();
+	return userArray;
 }
 }
